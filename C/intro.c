1213de7dcc646997099b77efa440fef6785d6c74
PristarSingh <50176827+PristarSingh@users.noreply.github.com>
2021-04-05 19:35:29 +0530

    first commit

---
+++ C/intro.c
@@ -0,0 +1,8 @@
+/*
+
+#include<stdio.h>: header file  is compulsary
+main(): function is the only function which execute in program
+printf(): to print something
+scanf(): to take input from user 
+
+*/
