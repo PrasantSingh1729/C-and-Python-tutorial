386db09adba1e77ff605ffc1f8d31e654d5c64e9
PristarSingh <50176827+PristarSingh@users.noreply.github.com>
2021-04-05 19:40:30 +0530

    day 1

--- C/intro.c
+++ C/intro.c
@@ -6,3 +6,15 @@ printf(): to print something
 scanf(): to take input from user 
 
 */
+
+
+#include<stdio.h>
+
+void main(){
+    int a,b;
+    printf("Enter first number: ");
+    scanf("%d",a);
+    printf("Enter second number: ");
+    scanf("%d",b);
+    printf("Sum of number is: %d",a+b);
+}
